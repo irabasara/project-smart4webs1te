5ad663a79bf6f25d7baf5d17ec5a9a26ec5e9d50
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-08-04 00:05:56 +0200

    finish supportUkrain

--- src/js/supportUkraine.js
+++ src/js/supportUkraine.js
@@ -43,7 +43,7 @@ const FONDS = [
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
     img: null,
-  }
+  },
 ];
 
 const el = {
@@ -52,6 +52,17 @@ const el = {
   btn: document.querySelector('.support-ukraine-btn'),
 };
 
+let options = {
+  root: el.list.parentElement,
+  rootMargin: '0px',
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(observerHandler, options);
+
+function observerHandler() {
+  el.list.parentElement.addEventListener('scroll', handleOnScroll);
+}
 function createMarcap() {
   return FONDS.map(
     (obj, i) =>
@@ -74,21 +85,57 @@ el.list.insertAdjacentHTML('beforeend', createMarcap());
 
 window.addEventListener('resize', handleViewportChange);
 
+function handleChangeHeight(e) {
+  el.list.parentElement.classList.toggle('active');
+  if (e.currentTarget.firstChild.classList.contains('skroll-active')) {
+    e.currentTarget.firstChild.classList.remove('skroll-active');
+  } else {
+    e.currentTarget.firstChild.classList.add('skroll-active');
+  }
+}
+
 function handleScrolldown(e) {
-      e.currentTarget.firstChild.classList.toggle('skroll-active');
-      el.list.classList.toggle('active');
-};
-    
+  if (!e.currentTarget.firstChild.classList.contains('skroll-active')) {
+    el.list.parentElement.removeEventListener('scroll', handleOnScroll);
+    e.currentTarget.firstChild.classList.toggle('skroll-active');
+    el.list.parentElement.scrollTo({
+      top: el.list.scrollHeight,
+      behavior: 'smooth',
+    });
+  } else {
+    el.list.parentElement.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+}
+
+function handleOnScroll() {
+  if (window.innerWidth >= 768 && window.innerWidth <= 1279) {
+    if (el.list.getBoundingClientRect().top > 100) {
+      el.btn.firstChild.classList.remove('skroll-active');
+    } else {
+      el.btn.firstChild.classList.add('skroll-active');
+    }
+  }
+}
+
 function handleViewportChange() {
-  console.log(window.innerWidth);
   checkViewportSize();
-};
+}
 
 function checkViewportSize() {
-  if (window.innerWidth < 768) {
-    el.btn.addEventListener('click', handleScrolldown);
+  if (window.innerWidth < 768 || window.innerWidth > 1279) {
+    el.btn.firstChild.classList.remove('skroll-active');
+    el.list.parentElement.classList.remove('active');
+    el.btn.addEventListener('click', handleChangeHeight);
+    el.btn.removeEventListener('click', handleScrolldown);
+    observer.unobserve(el.list.children[el.list.children.length - 1]);
   } else {
-      el.btn.removeEventListener('click', handleScrolldown)
-    }
-};
+    el.btn.removeEventListener('click', handleChangeHeight);
+    el.btn.addEventListener('click', handleScrolldown);
+    el.list.parentElement.addEventListener('scroll', handleOnScroll);
+    observer.observe(el.list.children[el.list.children.length - 1]);
+  }
+}
 checkViewportSize();
