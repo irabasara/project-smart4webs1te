6f2cfceb4bb56085cbbbdc26c16a6b9bdbcd9b0f
ihorkoi <koleev.igor@gmail.com>
2023-08-04 15:02:01 +0300

    update card

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -5,15 +5,21 @@ export function generateBookCardMarkup(booksArray) {
       <div class="shopping-image-thumb">
         <img src="${book_image}" alt="" />
       </div>
-      <div class="">
-      <div class="book-description">
+      <div class="book-card-info">
+      <div class="book">
         <h2 class="book-title">${title}</h2>
         <div class="book-genre">${list_name}</div>
-        <div class="book-description">afdexzveawf<axc${description}</div>
-        <img src="" alt="" />
+        <div class="book-description">${
+          description
+            ? description
+            : "We don't have description for this book yet..."
+        }</div>
+        <svg class="book-card-icon">
+        <use src="../img/sprite.svg#icon-trash" alt="" />
+        </svg>
       </div>
       <div>
-        <div class="book-genre">${author}</div>
+        <div class="book-author">${author}</div>
         <a href=""><img src="" alt="" /></a>
         <a href=""><img src="" alt="" /></a>
         <a href=""><img src="" alt="" /></a>
@@ -30,3 +36,7 @@ fetch('https://books-backend.p.goit.global/books/643282b1e85766588626a0dc')
   .then(response => response.json())
   .then(book => (localStorage['book'] = JSON.stringify(book)));
 console.log(generateBookCardMarkup([JSON.parse(localStorage['book'])]));
+
+if (!localStorage.getItem('book')) {
+  document.querySelector('.empty-list').removeAttribute('display');
+}
