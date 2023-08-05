0b0c3e0610e201d8ffbd40366bc78a8b87b964ba
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2023-08-06 01:13:02 +0300

    previous version2

--- src/js/bestSellerBooks.js
+++ src/js/bestSellerBooks.js
@@ -3,37 +3,44 @@ import { getBooksAPI } from "./getBoorkAPI";
 const bestBooks = document.querySelector('.home-title-book');
 const container = document.querySelector('.js-container-bestBooks');
 
+
 bestBooks.addEventListener('click', onClickBooks);
 
 function onClickBooks() {
   // try {
   //   const data = await getBooksAPI('top-books');
   //   console.log(data)
-  //   { data } = data.map(({ list_name, books }) => {
+  //   const info = data.map(({ list_name, books }) => {
   //     console.log('list_name', list_name);
   //     console.log('books', books);
-      // let { list_name, books } = data;
-      // console.log('list_name', data[0].list_name)
-      // console.log('books', books)
-    // catch (error) {
+  //     markupList(info)
+  //   })  // }
+  //   catch(error) {
   //     console.error(error)
-  //   }
-  // }
-
+  //   }}
   getBooksAPI('top-books')
-  .then(({ data }) => data.map(({ list_name, books }) => {
-    console.log('list_name', list_name);
-    console.log('books', books);
-    const markupList = books.map(({ book_image, title, author }) => {
-     return `<li class="js-list-bestBooks">
-            <p class="js-list-name">${list_name}</p>
-            <img src="${book_image}" alt="${title}" loading="lazy" class="img-bestBooks" width="335" height="485"/>
+    .then(({ data }) => data.map(({ list_name, books }) => {
+      console.log('list_name', list_name);
+      console.log('books', books);
+      const markupList = books.map(({ book_image, title, author, _id }) => {
+        return `<li class="js-list-bestBooks id=${_id}">
+            <img src="${book_image}" alt="${title}" loading="lazy" class="img-bestBooks"/>
             <h3 class="js-named-bestBooks">${title}</h3>
             <p class="js-autor-bestBooks">${author}</p>
-            <button class="js-btn-bestBooks">See more</button>
-        </li>`}).join('');
-  container.insertAdjacentHTML('beforeend', markupList)
-}))
+        </li>`});
+        container.insertAdjacentHTML('beforeend', markupList.join(''))
+      
+        
+        const markupBlock = data.map(({ list_name}) => {
+          return `<div class="js-container-homeBooks">
+        <h3 class="js-list-name">${list_name}</h3 >
+        <ul>${markupList}</ul>
+        <btn class="js-btn-bestBooks">See more</btn>
+        </div>`
+        }).join('');
+        container.insertAdjacentHTML('beforeend', markupBlock)
+        
+    }))
 
 }
 
