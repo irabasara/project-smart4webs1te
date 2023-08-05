4ed4dcb1fd5ce86c0126c6fd59a6093c291e3750
irabasara <125157770+irabasara@users.noreply.github.com>
2023-08-06 00:41:36 +0300

    add modal

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,7 @@
 export const refs = {
     scrollTop: document.querySelector('.scroll'),
+    openModalBtn: document.querySelector('#js-modal-open'),
+    closeModalBtn: document.querySelector('#js-modal-close'),
+    modal: document.querySelector('#js-modal'),
     
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import './js/getBoorkAPI'
-// import './js/loader'
-
 import './js/auth/authUser'
 import './js/auth/signIn'
 import './js/auth/submitForm'
 import './js/refs'
 import './js/supportUkraine'
 import './js/scroll-top'
+import './js/modal'
 
 
