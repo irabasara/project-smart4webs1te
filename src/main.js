c946f8dcd2cefb40250553a89b61dbfd214970c6
irabasara <125157770+irabasara@users.noreply.github.com>
2023-08-06 20:55:59 +0300

    fix pages

--- src/main.js
+++ src/main.js
@@ -7,17 +7,8 @@ import './js/supportUkraine';
 import './js/scroll-top';
 import './js/modal';
 import './js/shopping-list';
-
 import './js/getBoorkAPI'
-
-// import './js/loader'
 import './js/bestSellerBooks'
 
-import './js/auth/authUser'
-import './js/auth/submitForm'
-import './js/refs'
-import './js/supportUkraine'
-import './js/scroll-top'
-import './js/modal'
 
 
