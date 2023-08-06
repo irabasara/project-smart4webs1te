9ac174ecbf7c1c07c84f87ebc16425cece8d01e9
irabasara <125157770+irabasara@users.noreply.github.com>
2023-08-06 00:13:27 +0000

    Deploying to gh-pages from @ irabasara/project-smart4webs1te@79b6cd43c0180d30a73a1379aaa0184f508876f4 🚀

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-export const refs = {
-    scrollTop: document.querySelector('.scroll'),
-    openModalBtn: document.querySelector('#js-modal-open'),
-    closeModalBtn: document.querySelector('#js-modal-close'),
-    modal: document.querySelector('#js-modal'),
-    
-}
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './js/getBoorkAPI'
-import './js/auth/authUser'
-import './js/auth/submitForm'
-import './js/refs'
-import './js/supportUkraine'
-import './js/scroll-top'
-import './js/modal'
-
-
