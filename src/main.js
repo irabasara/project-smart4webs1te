2636c63e7e0e90910d2f474fa9d2e8bbead4b6c7
ihorkoi <koleev.igor@gmail.com>
2023-08-07 19:55:30 +0300

    bugfix

--- src/main.js
+++ src/main.js
@@ -10,24 +10,23 @@ import './js/shopping-list';
 import './js/getBoorkAPI';
 import './js/theme-switcher';
 
-import './js/getBoorkAPI'
+import './js/getBoorkAPI';
 
-import './js/auth/authUser'
-import './js/auth/submitForm'
-import './js/refs'
-import './js/supportUkraine'
-import './js/scroll-top'
+import './js/auth/authUser';
+import './js/auth/submitForm';
+import './js/refs';
+import './js/supportUkraine';
+import './js/scroll-top';
 
-import './js/modal-book'
+import './js/modal-book';
 // import './js/loader'
 
-import './js/modal'
-import './js/renderAllCategories'
+import './js/modal';
+import './js/renderAllCategories';
 
-import './js/mob-menu'
-import './js/bestSellerBooks'
-import './js/header'
-
+import './js/mob-menu';
+import './js/bestSellerBooks';
+import './js/header';
 
 import './js/auth/authUser';
 import './js/auth/submitForm';
@@ -37,8 +36,6 @@ import './js/scroll-top';
 import './js/modal';
 import './js/renderAllCategories';
 
-
 import './js/mob-menu';
 import './js/bestSellerBooks';
 import './js/header';
-
