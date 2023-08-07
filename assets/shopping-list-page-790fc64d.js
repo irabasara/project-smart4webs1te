5877369085c7bc83f976d601bba4385bc1234d3f
irabasara <125157770+irabasara@users.noreply.github.com>
2023-08-07 16:23:54 +0000

    Deploying to gh-pages from @ irabasara/project-smart4webs1te@bfcff634f5e10b9d32ec4d4d1130bcb6698a6fb4 🚀

--- assets/shopping-list-page-790fc64d.js
+++ assets/shopping-list-page-790fc64d.js
@@ -10,17 +10,17 @@ import"./mob-menu-8394a373.js";function u(e){let o='<ul class="shopping-list">';
         
         <button class="book-card-delete">
         <svg class="book-card-icon">
-        <use href="../img/sprite.svg#icon-trash"></use>
+        <use href="./img/sprite.svg#icon-trash"></use>
         </svg>
         </button>
       </div>
       <div class="book-card-refs">
         <div class="book-author">${n}</div>
         <a class="amazon-icon" href="${d}" target="_blank">
-        <img src="../img/amazon.svg" alt="" / >
+        <img src="./img/amazon.svg" alt="" / >
         </a>
-        <a class="other-icon" href="${g}" target="_blank"><img src="../img/applebook.svg" alt="" / ></a>
-        <a class="other-icon" href="${p}" target="_blank" ><img src="../img/bookshop.svg" alt="" /></a>
+        <a class="other-icon" href="${g}" target="_blank"><img src="./img/applebook.svg" alt="" / ></a>
+        <a class="other-icon" href="${p}" target="_blank" ><img src="./img/bookshop.svg" alt="" /></a>
       </div>
       </div>
     </li>`}),o+="</ul>",document.querySelector(".container").insertAdjacentHTML("beforeend",o),document.querySelector(".shopping-list").addEventListener("click",m)}function r(e,o){return o.filter(t=>{if(t.name===e)return t.url})[0].url}function m(e){if(e.target.nodeName!=="BUTTON")return;let o=e.target;for(;o&&!o.hasAttribute("id");)o=o.parentNode;o.remove(),document.querySelector(".shopping-list").children.length<1&&(document.querySelector(".empty-list").style.display="flex"),h(o.id,localStorage.getItem("shoppingList"))}function h(e,o){const i=JSON.parse(o).filter(t=>{if(t.title!==e)return t});localStorage.setItem("shoppingList",JSON.stringify(i))}localStorage.shoppingList.length>2&&(document.querySelector(".empty-list").style.display="none");u(JSON.parse(localStorage.shoppingList));
