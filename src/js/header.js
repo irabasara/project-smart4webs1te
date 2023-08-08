a62755d2cd0d4fa7bd97dc1e2a83ba839bc374b4
irabasara <125157770+irabasara@users.noreply.github.com>
2023-08-08 15:54:15 +0300

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -2,13 +2,12 @@
 import { refs } from "./refs"
 
 const location = window.location.pathname
-console.log('location', location)
-if (location === '/index.html') {
+if (location === '/project-smart4webs1te/index.html') {
     refs.homeLink.classList.toggle('active')
     refs.homeLinkMobile.classList.toggle('active')
 
 }
-if (location === '/shopping-list-page.html') {
+if (location === '/project-smart4webs1te/shopping-list-page.html') {
     refs.shopLink.classList.toggle('active')
     refs.shopLinkMobile.classList.toggle('active')
 
