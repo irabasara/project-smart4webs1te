df68ca0378d76a0c15883922b48aeba7e7b23c08
irabasara <125157770+irabasara@users.noreply.github.com>
2023-08-09 17:29:02 +0300

    add loader

--- src/js/getBoorkAPI.js
+++ src/js/getBoorkAPI.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
+import { Loading } from 'notiflix';
 
 axios.defaults.baseURL = 'https://books-backend.p.goit.global/books/';
 
 export async function getBooksAPI(endPoint) {
+  if (endPoint = 'top-books') {
+  Loading.pulse();
+    Loading.remove(3000)
+  } else {
+  Loading.pulse();
+  }
   try {
     const response = await axios.get(`${endPoint}`);
+    Loading.remove()
     return response;
   } catch (error) {
+    Loading.remove()
+
     console.error(error);
   }
 }
+
+
+// console.log('getBooksAPI()', getBooksAPI())
 
 // import { getBooksAPI } from './js/getBoorkAPI';
 
